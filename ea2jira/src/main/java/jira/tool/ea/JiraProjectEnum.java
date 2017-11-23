2347c3708115bc0b7ab5bb4dc400961767998d09
DING Xianli <ylpdxl@hotmail.com>
2017-11-23 13:16:04 +0800

    Start to refactor ea2jira: process the whole file directly. Don't depend on the transfer.eap

---
+++ ea2jira/src/main/java/jira/tool/ea/JiraProjectEnum.java
@@ -0,0 +1,62 @@
+package jira.tool.ea;
+
+import dbtools.common.utils.ArrayUtils;
+import dbtools.common.utils.StrUtils;
+
+public enum JiraProjectEnum {
+    DeSheng("A-德升", new String[]{"德升"}),
+    HaiHang("A-海航", new String[]{"海航"}),
+    JingKeLong("A-京客隆", new String[]{"京客隆"}),
+    OuPu("A-欧普照明", new String[]{"欧普"}),
+    PingAn("A-平安租赁", new String[]{"平安"}),
+    ShiTaiBo("A-史泰博", new String[]{"史泰博"}),
+
+    TechDebt("D-技术优化", new String[]{"大数据", "交易线", "用户线"});
+
+    private static JiraProjectEnum[] projectArray = new JiraProjectEnum[]{
+            DeSheng, HaiHang, JingKeLong, OuPu, PingAn, ShiTaiBo,
+            TechDebt,
+    };
+
+    public static JiraProjectEnum findProject(String fileName) {
+        if (StrUtils.isEmpty(fileName) || projectArray == null || projectArray.length <= 0) {
+            return null;
+        }
+
+        String name = fileName.toLowerCase();
+        for (JiraProjectEnum project : projectArray) {
+            if (name.startsWith(project.getName().toLowerCase())) {
+                return project;
+            }
+
+            String[] prefixes = project.getPrefixes();
+            if (ArrayUtils.isEmpty(prefixes)) {
+                continue;
+            }
+
+            for (String prefix : prefixes) {
+                if (name.startsWith(prefix.toLowerCase())) {
+                    return project;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private String name;
+    private String[] prefixes;
+
+    JiraProjectEnum(String name, String[] prefixes) {
+        this.name = name;
+        this.prefixes = prefixes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getPrefixes() {
+        return prefixes;
+    }
+}
