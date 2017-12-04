c850baa6df2818b646fc96dcccdb16ce0a1418fb
DING Xianli <ylpdxl@hotmail.com>
2017-12-04 16:55:46 +0800

    Update jira

--- jira2ea/src/main/java/jira/tool/jira/JiraStatusEnum.java
+++ jira2ea/src/main/java/jira/tool/jira/JiraStatusEnum.java
@@ -1,10 +1,11 @@
 package jira.tool.jira;
 
+import dbtools.common.utils.ArrayUtils;
 import dbtools.common.utils.StrUtils;
 
 public enum JiraStatusEnum {
     Pending("PENDING"),
-    Closed("关闭");
+    Closed("关闭", new String[]{});
 
     public static boolean isClosed(String status) {
         if (StrUtils.isEmpty(status)) {
@@ -16,13 +17,27 @@ public enum JiraStatusEnum {
             if (processStatus.code.equalsIgnoreCase(status)) {
                 return true;
             }
+
+            if (!ArrayUtils.isEmpty(processStatus.aliases)) {
+                for (String alias : processStatus.aliases) {
+                    if (alias.equalsIgnoreCase(status)) {
+                        return true;
+                    }
+                }
+            }
         }
         return false;
     }
 
     private String code;
+    private String[] aliases;
 
     JiraStatusEnum(String code) {
         this.code = code;
+    }
+
+    JiraStatusEnum(String code, String[] aliases) {
+        this.code = code;
+        this.aliases = aliases;
     }
 }
--- jira2ea/src/test/java/jira/tool/jira/JiraStatusEnumTest.java
+++ jira2ea/src/test/java/jira/tool/jira/JiraStatusEnumTest.java
@@ -9,14 +9,15 @@ import java.util.Map;
 public class JiraStatusEnumTest {
     @Test
     public void testIsClosed() {
-        Map<String, Boolean> mapIO = new HashMap<String, Boolean>(){{
-           put(null, false);
-           put("", false);
-           put("test", false);
-           put("pending", true);
-           put("Pending", true);
-           put("PENDING", true);
-           put("关闭", true);
+        Map<String, Boolean> mapIO = new HashMap<String, Boolean>() {{
+            put(null, false);
+            put("", false);
+            put("test", false);
+            put("pending", true);
+            put("Pending", true);
+            put("PENDING", true);
+            put("关闭", true);
+            put("Done", false);
         }};
 
         for (Map.Entry<String, Boolean> io : mapIO.entrySet()) {
