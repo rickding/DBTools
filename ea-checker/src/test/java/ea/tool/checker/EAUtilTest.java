ab622384ff2579f25d944933634141defc955050
DING Xianli <ylpdxl@hotmail.com>
2017-12-06 17:12:41 +0800

    Support to read eap file

---
+++ ea-checker/src/test/java/ea/tool/checker/EAUtilTest.java
@@ -0,0 +1,17 @@
+package ea.tool.checker;
+
+import ea.tool.api.EAUtil;
+import org.junit.Test;
+
+import java.util.List;
+
+public class EAUtilTest {
+    @Test
+    public void testGetElementList() {
+//        System.out.println("Start...");
+//
+//        String file = "C:\\Work\\dbtools\\dbtools\\sample\\sample.EAP";
+//        List<String[]> elements = EAUtil.getElementList(file);
+//        System.out.printf("%s, elements: %d, %s\r\n", file, elements.size(), elements);
+    }
+}
