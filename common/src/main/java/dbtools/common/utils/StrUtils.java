a1a9132b5145f44cc211e1d262ee2017847ffc75
DING Xianli <ylpdxl@hotmail.com>
2017-12-07 17:18:26 +0800

    Add StrUtils.split and combine

--- common/src/main/java/dbtools/common/utils/StrUtils.java
+++ common/src/main/java/dbtools/common/utils/StrUtils.java
@@ -5,6 +5,28 @@ package dbtools.common.utils;
  */
 public class StrUtils {
     public static boolean isEmpty(String str) {
-        return str == null || str.length() <= 0;
+        return str == null || str.trim().length() <= 0;
+    }
+
+    public static String[] split(String str, String separator) {
+        if (StrUtils.isEmpty(str) || separator == null || separator.length() <= 0) {
+            return null;
+        }
+
+        return str.split(separator);
+    }
+
+    public static String combine(String[] strArray, String separator) {
+        if (ArrayUtils.isEmpty(strArray) || separator == null) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArray) {
+            sb.append(separator);
+            sb.append(str);
+        }
+
+        return sb.substring(separator.length());
     }
 }
--- common/src/test/java/dbtools/common/utils/StrUtilsTest.java
+++ common/src/test/java/dbtools/common/utils/StrUtilsTest.java
@@ -23,4 +23,33 @@ public class StrUtilsTest {
             Assert.assertEquals(io.getValue(), ret);
         }
     }
+
+    @Test
+    public void testSplit() {
+        Map<String, String[]> mapIO = new HashMap<String, String[]>() {{
+            put(null, null);
+            put("", null);
+            put("t", new String[] {"t"});
+            put("t,a", new String[] {"t", "a"});
+        }};
+
+        for (Map.Entry<String, String[]> io : mapIO.entrySet()) {
+            String[] ret = StrUtils.split(io.getKey(), ",");
+            Assert.assertArrayEquals(io.getValue(), ret);
+        }
+    }
+
+    @Test
+    public void testCombine() {
+        Map<String, String[]> mapIO = new HashMap<String, String[]>() {{
+            put(null, null);
+            put("t", new String[] {"t"});
+            put("t, a", new String[] {"t", "a"});
+        }};
+
+        for (Map.Entry<String, String[]> io : mapIO.entrySet()) {
+            String ret = StrUtils.combine(io.getValue(), ", ");
+            Assert.assertEquals(io.getKey(), ret);
+        }
+    }
 }
