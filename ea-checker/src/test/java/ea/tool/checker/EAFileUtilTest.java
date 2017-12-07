36bec853f5e7b097f56cf77d0289442e0f9dbd09
DING Xianli <ylpdxl@hotmail.com>
2017-12-07 11:03:37 +0800

    Refactor file names and start to add ea headers

---
+++ ea-checker/src/test/java/ea/tool/checker/EAFileUtilTest.java
@@ -0,0 +1,14 @@
+package ea.tool.checker;
+
+import org.junit.Test;
+
+public class EAFileUtilTest {
+    @Test
+    public void testGetElementList() {
+//        System.out.println("Start...");
+//
+//        String file = "C:\\Work\\dbtools\\dbtools\\sample\\sample.EAP";
+//        List<String[]> elements = EAFileUtil.getElementList(file);
+//        System.out.printf("%s, elements: %d, %s\r\n", file, elements.size(), elements);
+    }
+}
