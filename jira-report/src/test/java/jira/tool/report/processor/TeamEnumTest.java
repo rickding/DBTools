0ea9dba92c8c298f3fcfa279653b9c26f6ec34da
DING Xianli <ylpdxl@hotmail.com>
2017-12-08 17:20:09 +0800

    Read team member from db

---
+++ jira-report/src/test/java/jira/tool/report/processor/TeamEnumTest.java
@@ -0,0 +1,10 @@
+package jira.tool.report.processor;
+
+import org.junit.Test;
+
+public class TeamEnumTest {
+    @Test
+    public void testCheckTeamMemberCount() {
+        TeamEnum.checkTeamMemberCount();
+    }
+}
