a5a2b7ce290d7b6b2f50a079f0f2033f3df9f90e
DING Xianli <ylpdxl@hotmail.com>
2018-01-05 11:54:47 +0800

    rms api

---
+++ rms-api/src/main/java/com/hello/springboot/controller/RmsApiController.java
@@ -0,0 +1,23 @@
+package com.hello.springboot.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by user on 2017/5/28.
+ */
+@RestController
+@EnableAutoConfiguration
+public class RmsApiController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "Hello RMS API";
+    }
+
+    @RequestMapping(value = "/chk", method = RequestMethod.GET)
+    public String chk() {
+        return "ok";
+    }
+}
